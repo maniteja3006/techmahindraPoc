9d8a75da325dc07d6e0c232cbcb5691b13dbbddd
mani-tech <devarakondamaniteja112@gmail.com>
2022-05-02 14:02:59 +0530

    Added swagger

--- src/main/java/com/techmahindra/practise/PractiseApplication.java
+++ src/main/java/com/techmahindra/practise/PractiseApplication.java
@@ -3,6 +3,7 @@ package com.techmahindra.practise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PractiseApplication {
 
@@ -10,4 +11,5 @@ public class PractiseApplication {
 		SpringApplication.run(PractiseApplication.class, args);
 	}
 
+	
 }
