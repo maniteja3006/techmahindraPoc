6653bd456b7e4b4243a7c974169f3edff8dbd341
mani-tech <devarakondamaniteja112@gmail.com>
2022-05-03 13:14:30 +0530

    changed comment

--- src/main/java/com/techmahindra/controller/EmployeeController.java
+++ src/main/java/com/techmahindra/controller/EmployeeController.java
@@ -28,7 +28,7 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
-	// get all employees
+	// get all employee
 	@GetMapping("/employees")
 	public List<Employee> getAllEmployees(){
 		return employeeRepository.findAll();
